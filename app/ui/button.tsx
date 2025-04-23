9b16597c41821297b22c75e477a5401166fc3c27
Abdulrahman Moussa <abdelrahmanmoussa111@gmail.com>
2025-04-23 14:31:00 +0200

    enhcne shine button

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -20,7 +20,7 @@ const Button = ({
 }: ButtonProps) => {
   const variantStyles = {
     primary:
-      'bg-primary-light dark:bg-primary-dark text-text-dark dark:text-text-light rounded-lg relative z-[1] px-3 py-2 overflow-hidden after:absolute after:-top-1/2 after:-bottom-1/2 after:left-0 after:w-5 after:content-[""] after:z-[-1] after:bg-[#ffffff33] shine',
+      'bg-primary-light dark:bg-primary-dark text-text-dark dark:text-text-light rounded-lg relative z-[1] px-3 py-2 overflow-hidden shine',
     secondary: '',
     tertiary:
       "relative before:content-[''] before:absolute before:bottom-0 before:left-0 before:right-0 before:h-[2px] before:bg-primary-light dark:before:bg-primary-dark before:origin-bottom-right before:scale-x-0 before:transition-transform before:duration-500 hover:before:origin-bottom-left hover:before:scale-x-100",
