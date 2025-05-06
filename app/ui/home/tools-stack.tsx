ca30ddab8c07ff505664dc548ac380b471701d3f
Abdulrahman Moussa <abdelrahmanmoussa111@gmail.com>
2025-05-06 20:06:36 +0300

    fix tools-stack.tsx:98:35 erorr, enhance the file

--- app/ui/home/tools-stack.tsx
+++ app/ui/home/tools-stack.tsx
@@ -1,87 +1,5 @@
-import { FaFigma, FaTerminal } from 'react-icons/fa6';
-import {
-  SiVim,
-  SiPostman,
-  SiNetlify,
-  SiVercel,
-  SiAxios,
-  SiMaterialdesign,
-  SiVitest,
-  SiDaisyui,
-  SiI18Next,
-} from 'react-icons/si';
-import { DiGit, DiResponsive } from 'react-icons/di';
-import { TbBrandGithubCopilot } from 'react-icons/tb';
-import { PiFramerLogoFill } from 'react-icons/pi';
-import { VscVscode } from 'react-icons/vsc';
 import TechCircle from './tech-circle';
-
-const Tools = [
-  {
-    name: 'Visual Studio Code',
-    icon: <VscVscode />,
-  },
-  {
-    name: 'Terminal',
-    icon: <FaTerminal />,
-  },
-  {
-    name: 'Vim',
-    icon: <SiVim />,
-  },
-  {
-    name: 'Github Copilot',
-    icon: <TbBrandGithubCopilot />,
-  },
-  {
-    name: 'Git',
-    icon: <DiGit />,
-  },
-  {
-    name: 'Postman',
-    icon: <SiPostman />,
-  },
-  {
-    name: 'Axios',
-    icon: <SiAxios />,
-  },
-  {
-    name: 'i18next',
-    icon: <SiI18Next />,
-  },
-  {
-    name: 'Figma',
-    icon: <FaFigma />,
-  },
-  {
-    name: 'Netlify',
-    icon: <SiNetlify />,
-  },
-  {
-    name: 'Vercel',
-    icon: <SiVercel />,
-  },
-  {
-    name: 'Material UI',
-    icon: <SiMaterialdesign />,
-  },
-  {
-    name: 'Daisy UI',
-    icon: <SiDaisyui />,
-  },
-  {
-    name: 'Framer Motion',
-    icon: <PiFramerLogoFill />,
-  },
-  {
-    name: 'Responsive Design',
-    icon: <DiResponsive />,
-  },
-  {
-    name: 'Vite',
-    icon: <SiVitest />,
-  },
-];
+import { toolsStack } from '@/app/data/tech-tool';
 
 interface ToolsStackProps {
   children: React.ReactNode;
@@ -94,8 +12,14 @@ const ToolsStack = ({ children, padding, size }: ToolsStackProps) => {
     <div className="flex flex-col gap-y-4 w-full max-w-md">
       {children}
       <div className="grid grid-cols-5 lg:grid-cols-6 gap-4">
-        {Tools.map((tool, index) => (
-          <TechCircle key={index} skill={tool} padding={padding} size={size} />
+        {toolsStack.map((item, index) => (
+          <TechCircle
+            key={index}
+            name={item.name}
+            icon={<item.icon />}
+            padding={padding}
+            size={size}
+          />
         ))}
       </div>
     </div>
